13e660fa6e4ea6b1de48d5e712fa4d494d5d1df4
frontdev1987 <119827017+frontdev1987@users.noreply.github.com>
2024-10-10 22:48:22 +0000

    almost done

--- src/js/app.js
+++ src/js/app.js
@@ -33,9 +33,9 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
+          <h1>Michael Sheppard</h1>
           <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h3>Bronx,USA</h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
@@ -56,7 +56,7 @@ window.onload = function() {
     // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "./IMG-20240411-WA0017.jpg",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
